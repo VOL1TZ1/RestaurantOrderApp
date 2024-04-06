149cf965dfffc2f582ea7a953129f80b2ef15e85
VOL1TZ1 <91470979+VOL1TZ1@users.noreply.github.com>
2024-04-06 12:38:34 +0200

    Added the item class to the Project Tested the inti of an item's object in the main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
-
 public class Main {
     public static void main(String[] args) {
         System.out.print("\t\t\t\t\t\t\t\t\t\t");
         System.out.println("!!!! Hello and welcome to Sprints Ordering App in Java !!!!");
+
+        item testItem =new item("Dish 1","Dish description",300);
+        System.out.println(STR."Dish name = \{testItem.getName()}");
+        System.out.println(STR."Dish description = \{testItem.getDescription()}");
+        System.out.println(STR."Dish price = \{testItem.getPrice()}");
     }
 }
---
+++ src/item.java
@@ -0,0 +1,31 @@
+public class item {
+    //The Class Variables:
+    private String name, description;
+    private double price;
+
+    //Default constructor:
+    public item() {
+        this.name="Dish";
+        this.description="A dish";
+        this.price=0;
+    }
+
+    //Constructor for entering all item's data:
+    public item(String dName ,String dDescription, double dPrice){
+        this.name=dName;
+        this.description=dDescription;
+        this.price=dPrice;
+    }
+
+    //The "Get" methods:
+    void setName(String name){this.name=name;}
+    void setDescription(String description){this.description=description;}
+    void setPrice(double price){this.price=price;}
+
+    //The "Set" methods:
+    String getName(){return this.name;}
+    String getDescription(){return this.description;}
+    double getPrice(){return this.price;}
+
+
+}

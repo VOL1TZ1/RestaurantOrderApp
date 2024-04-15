ab0a33f29803632182a2f338c31e1ebbe0f6ba61
VOL1TZ1 <91470979+VOL1TZ1@users.noreply.github.com>
2024-04-15 09:23:13 +0200

    Started in the checkout process

--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,93 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
-    public static void main()  {
+    public static void main() {
         //Making an object of Order:
-        Order userOrder =new Order();
-        boolean infLoop =true;
+        Order userOrder = new Order();
+        boolean infLoop = true;
         //Intro to the program:
         System.out.print("\t\t\t\t\t\t\t\t\t\t");
         System.out.println("!!!! Hello and welcome to Sprints Ordering App in Java !!!!");
 
-        while (infLoop){
-        //Choosing the dish:
-        System.out.print("choose the menu: ");
-        Scanner sMenu= new Scanner(System.in);
-        int categoryNum=sMenu.nextInt();    //keeping track of which menu to use
+        while (infLoop) {
+            //Choosing the dish:
+            System.out.print("choose the menu: ");
+            Scanner sMenu = new Scanner(System.in);
+            int categoryNum = sMenu.nextInt();    //keeping track of which menu to use
 
-        while(categoryNum>3||categoryNum<1){    //a loop to make sure the user choose a valid menu number
-            System.out.print("choose the menu from (1-3): ");
-            categoryNum=sMenu.nextInt();
-        }
-        Menu userMenu = new Menu(categoryNum);
-        userMenu.getDishMenu(); //a method to show all the dishes in the menu
+            while (categoryNum > 3 || categoryNum < 1) {    //a loop to make sure the user choose a valid menu number
+                System.out.print("choose the menu from (1-3): ");
+                categoryNum = sMenu.nextInt();
+            }
+            Menu userMenu = new Menu(categoryNum);
+            userMenu.getDishMenu(); //a method to show all the dishes in the menu
 
-        System.out.print("choose the item from (1-7): ");
-        int dishNum=sMenu.nextInt();    //keeping track of dish number
-
-        while(dishNum>7||dishNum<1){    //a loop to make sure the user choose a valid dish number
             System.out.print("choose the item from (1-7): ");
-            dishNum=sMenu.nextInt();
-        }
+            int dishNum = sMenu.nextInt();    //keeping track of dish number
+
+            while (dishNum > 7 || dishNum < 1) {    //a loop to make sure the user choose a valid dish number
+                System.out.print("choose the item from (1-7): ");
+                dishNum = sMenu.nextInt();
+            }
 
 //        //making a dish object to store the user's choice and adding it to the cart:
 //        Dish userDish = userMenu.menu.get(dishNum);
 //        userOrder.addCart(userDish);
 
-        //show the dish info
-        userMenu.getDishInfo(dishNum);
-        System.out.printf("do you like to add the dish to the cart? (Y/N): ");
-        Scanner addChoice =new Scanner(System.in);
-        char choice =addChoice.next().charAt(0);
-        switch (choice){
-            case 'y':
-            case 'Y':
-            {
-                Dish userDish = userMenu.menu.get(dishNum);
-                userOrder.addCart(userDish);
-                userOrder.showCart();
-                System.out.printf("want to keep browsing or checkout ?:");
-                String cartChoice =addChoice.next();
-                if (cartChoice != "browsing" && cartChoice != "continue") {
-                    infLoop = false;
+            //show the dish info
+            userMenu.getDishInfo(dishNum);
+            System.out.printf("do you like to add the dish to the cart? (Y/N): ");
+            Scanner addChoice = new Scanner(System.in);
+            char choice = addChoice.next().charAt(0);
+            switch (choice) {
+                case 'y':
+                case 'Y': {
+                    Dish userDish = userMenu.menu.get(dishNum);
+                    userOrder.addCart(userDish);
+                    userOrder.showCart();
+                    System.out.printf("want to keep browsing or checkout ?:");
+                    String cartChoice = addChoice.next();
+                    if (Objects.equals(cartChoice, "checkout")) { //using Objects.equals to make it null safe
+                        infLoop = false;
+                    }
+                    break;
                 }
-                break;
-            }
-            case 'n':
-            case 'N':{continue;}
-            default:{
-                System.out.printf("unexpected error");
-            }
+                case 'n':
+                case 'N': {
+                    continue;
+                }
+                default: {
+                    System.out.printf("unexpected error");
+                }
 
 
+            }
         }
+
+        while (!infLoop) {
+
+            userOrder.finalCart();
+            System.out.print("want to make any changes (Y/N): ");
+            Scanner addChoice = new Scanner(System.in);
+            char choice = addChoice.next().charAt(0);
+            switch (choice) {
+                case 'y':
+                case 'Y': {
+                }
+                case 'n':
+                case 'N': {
+                    infLoop = true;
+                }
+                default: {
+                    System.out.print("unexpected error");
+                }
+
+
+            }
 
         }
 
 
     }
-
-
 }
--- src/Order.java
+++ src/Order.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 public class Order {
     ArrayList<Dish> cart = new ArrayList<>(); //list of dishes to act as a cart.
     double total=0; // a variable the total will be calculated in.
+    double taxPercent = 0.12; //taxes is 12%
+    double tax;
+    double totalAfterTax;
 
     public void addCart(Dish cDish){
         cart.add(cDish);
@@ -16,6 +19,16 @@ public class Order {
         for (int i = 0; i < cart.size(); i++){
             System.out.println(STR."\{i + 1}.\{cart.get(i).getName()}: \{cart.get(i).getPrice()}");
         }
-        System.out.println(STR."The total: \{total}");
+        System.out.printf("The total: %.2f \n" ,total);
+    }
+    public void finalCart(){
+        for (int i = 0; i < cart.size(); i++){
+            System.out.println(STR."\{i + 1}.\{cart.get(i).getName()}: \{cart.get(i).getPrice()}");
+        }
+        System.out.printf("The order: %.2f \n" ,total);
+        System.out.printf("Tax: %.2f \n",total*taxPercent);
+        totalAfterTax=(total*taxPercent)+total;
+        System.out.printf("The total: %.2f \n" ,totalAfterTax);
+
     }
 }
